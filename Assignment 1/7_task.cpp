0a50fb7310dab372f2e5a1b53d7d94821b5a9e8f
Anuar <kacrdbgs1423@gmail.com>
2026-03-19 20:07:53 +0500

    .

---
+++ Assignment 1/7_task.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int countd(int a) {
+    if (a == 0) return 0;
+    return 1 + countd(a/10);
+}
+
+int main() {
+    int a;
+    cin >> a;
+
+    cout << countd(a);
+
+}
